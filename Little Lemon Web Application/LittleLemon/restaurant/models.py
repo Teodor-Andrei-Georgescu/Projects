2c180318e9f86fbee646c8f6f083a39fe849b284
andrei <georgescu.t.andrei@gmail.com>
2024-06-08 13:23:44 -0700

    added a Menu and Booking model and move databse from SQLite to MySQL

--- Little Lemon Web Application/LittleLemon/restaurant/models.py
+++ Little Lemon Web Application/LittleLemon/restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.DecimalField(max_digits=6,decimal_places=0)
+    #no_of_guests = models.IntegerField(max_length=6)
+    booking_date = models.DateField()
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.DecimalField(max_digits=5,decimal_places=0)
+    #inventory = models.IntegerField(max_length=5)
