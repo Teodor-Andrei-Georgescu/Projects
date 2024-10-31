24245f461aede1e1dff3a213c222abb78f0ae4fd
andrei <georgescu.t.andrei@gmail.com>
2024-10-31 13:24:30 -0700

    updated header so check for if user is authenticated(is logged in) and update the header tab accordingly -- still need to add log in and log out funcationality but for now just want to start getting all webpages ready

--- DataShieldMDWebApplication/DataShieldMD/DataShieldMDApp/urls.py
+++ DataShieldMDWebApplication/DataShieldMD/DataShieldMDApp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path,include
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('api-token-auth/',obtain_auth_token),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
