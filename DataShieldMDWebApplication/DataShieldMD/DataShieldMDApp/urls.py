1942ff57d5dd1c62bd4a7b30d6cb2d9e8142e505
andrei <georgescu.t.andrei@gmail.com>
2024-10-31 16:12:33 -0700

    created how we do it page, and made some more small changes to header and footer. Next thing to do will probably be the registration page or lookg into databases that support file uploads.

--- DataShieldMDWebApplication/DataShieldMD/DataShieldMDApp/urls.py
+++ DataShieldMDWebApplication/DataShieldMD/DataShieldMDApp/urls.py
@@ -5,8 +5,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('how-we-do-it/', views.how_we_do_it, name='how_we_do_it'),
     path('api-token-auth/',obtain_auth_token),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
 ]
--- DataShieldMDWebApplication/DataShieldMD/DataShieldMDApp/views.py
+++ DataShieldMDWebApplication/DataShieldMD/DataShieldMDApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def how_we_do_it(request):
+    return render(request, 'how_we_do_it.html')
